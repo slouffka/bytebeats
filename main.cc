b3f5bd9c770bf196ad52cd6a6c52476aa47e2a35
Vyacheslav Moskalev <slouffka@gmail.com>
2015-02-11 10:43:13 +0600

    Most pleasant melody by default

--- main.cc
+++ main.cc
@@ -18,14 +18,14 @@ int main()
 
         // bytebeats
         //
-        // uint8_t temp = (t*5&t>>7)|(t*3&t>>10);
+        uint8_t temp = (t*5&t>>7)|(t*3&t>>10);
         // uint8_t temp = ((t/2*(15&(0x234568a0>>(t>>8&28))))|t/2>>(t>>11)^t>>12)+(t/16&t&24);
         // uint8_t temp = (t&t>>8);
         // uint8_t temp = (t>>7|t|t>>6)*10+4*(t&t>>13|t>>6);
         // uint8_t temp = ((t<<1)^((t<<1)+(t>>7)&t>>12))|t>>(4-(1^7&(t>>19)))|t>>7;
         // uint8_t temp = (t>>6|t>>(t>>16))*10+((t>>11)&7);
         // uint8_t temp = t*5&(t>>7)|t*3&(t*4>>10);
-        uint8_t temp = ((t*(t>>8|t>>9)&46&t>>8))^(t&t>>13|t>>6);
+        // uint8_t temp = ((t*(t>>8|t>>9)&46&t>>8))^(t&t>>13|t>>6);
 
         std::cout << temp;
     }
